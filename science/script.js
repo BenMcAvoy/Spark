8c76a48d792da3d6bf8026568e260f187afb7836
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-11-29 00:31:00 +0000

    Implement the enable select button

--- science/script.js
+++ science/script.js
@@ -1,1 +1,9 @@
+import { execute } from '/static/executor.js';
+
 console.log("--- Loading Spark for Science ---");
+
+document.getElementById('selectTextButton').addEventListener('click', async function() {
+  execute(function() {
+    document.body.style.userSelect = "text";
+  })
+});
