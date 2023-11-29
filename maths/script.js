e2456f9f68a00f4f5807d32c29f1d1a3f1c3c624
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-11-29 22:10:27 +0000

    Add the enable select to Maths

--- maths/script.js
+++ maths/script.js
@@ -1,1 +1,9 @@
-console.log("--- Loading Spark for Maths ---");
+import { execute } from '/static/executor.js';
+
+console.log("Loading Spark for Maths");
+
+document.getElementById('selectTextButton').addEventListener('click', async function() {
+  execute(function() {
+    document.body.style.userSelect = "text";
+  })
+});
