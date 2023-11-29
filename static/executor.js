96e4712e15f58a131b85e09a734a2a9d3535d940
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-11-29 14:40:30 +0000

    Return result from execution

--- static/executor.js
+++ static/executor.js
@@ -1,7 +1,9 @@
 export async function execute(func) {
-  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  await chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    func: func,
-  });
+ const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+ const results = await chrome.scripting.executeScript({
+   target: { tabId: tab.id },
+   func: func,
+ });
+
+ return results[0].result;
 }
