4081be1ad98d7c2fd68297eab7d69b2aed41d68c
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-11-29 14:44:26 +0000

    Implement the text button

--- science/script.js
+++ science/script.js
@@ -1,9 +1,27 @@
 import { execute } from '/static/executor.js';
+import { showAlert } from '/static/modal.js';
 
-console.log("--- Loading Spark for Science ---");
+console.log("Loaded Spark for Science");
 
 document.getElementById('selectTextButton').addEventListener('click', async function() {
   execute(function() {
     document.body.style.userSelect = "text";
   })
+});
+
+document.getElementById('copyTextButton').addEventListener('click', async function() {
+  const result = await execute(function(showAlert) {
+    const paragraphs = document.querySelectorAll('[class*="_Question_"]>div>p');
+    const hasImage = paragraphs[0].innerHTML.includes("img");
+    const slicedParagraphs = [].slice.call(paragraphs, 1);
+
+    var extractedText = "";
+    slicedParagraphs.forEach((value) => {
+      extractedText += value.innerText;
+    })
+
+    return extractedText
+  });
+
+  showAlert(result);
 });
