7d79921d079010228793b19ad5ef8362fab15eb5
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-11-29 15:14:33 +0000

    Update the modal with the html file

--- static/modal.js
+++ static/modal.js
@@ -1,21 +1,28 @@
 export async function showAlert(message) {
- const w = await chrome.windows.getCurrent();
- const w2 = await chrome.windows.create({
-   url: `data:text/html,<title>Alert</title><p>${message}</p>`,
-   type: 'popup',
-   left: Math.floor(w.left + (w.width - 300) / 2),
-   top: Math.floor(w.top + (w.height - 150) / 2),
-   height: 150,
-   width: 300,
- });
+  const url = chrome.runtime.getURL('/static/modal.html');
 
- return new Promise(resolve => {
-   chrome.windows.onRemoved.addListener(onRemoved, {windowTypes: ['popup']});
-   function onRemoved(id) {
-     if (id === w2.id) {
-       chrome.windows.onRemoved.removeListener(onRemoved);
-       resolve();
-     }
-   }
- });
+  const html = await fetch(url)
+    .then((response) => response.text())
+    .then((html) => html.replace(/{message}/, message));
+
+  const w = await chrome.windows.getCurrent();
+  const w2 = await chrome.windows.create({
+    // url: `data:text/html,<title>Alert</title><p>${message}</p>`,
+    url: `data:text/html,${html}`,
+    type: 'popup',
+    left: Math.floor(w.left + (w.width - 300) / 2),
+    top: Math.floor(w.top + (w.height - 150) / 2),
+    height: 150,
+    width: 300,
+  });
+
+  return new Promise(resolve => {
+    chrome.windows.onRemoved.addListener(onRemoved, {windowTypes: ['popup']});
+    function onRemoved(id) {
+      if (id === w2.id) {
+        chrome.windows.onRemoved.removeListener(onRemoved);
+        resolve();
+      }
+    }
+  });
 }
