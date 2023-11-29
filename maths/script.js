5597beb152f750054ad29d3642cdd14ce845dd44
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-11-29 22:49:58 +0000

    Create a button to remove mathML spans

--- maths/script.js
+++ maths/script.js
@@ -7,3 +7,9 @@ document.getElementById('selectTextButton').addEventListener('click', async func
     document.body.style.userSelect = "text";
   })
 });
+
+document.getElementById('mathMLButton').addEventListener('click', async function() {
+  execute(function() {
+    document.querySelectorAll('span.katex-mathml').forEach(element => element.remove());
+  })
+});
