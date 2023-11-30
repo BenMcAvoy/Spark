c9792b23d0ca9a61ca85e8dc5f59da910a7544cd
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-11-30 01:43:56 +0000

    Refactor execution

--- science/script.js
+++ science/script.js
@@ -16,13 +16,9 @@ document.getElementById('getQuestion').addEventListener('click', async function(
   // TODO: Reduce selectiveness.
   // e.g. Don't select `Zoom`
   const result = await execute(function() {
-    let text = "";
+    const elements = [...document.querySelectorAll('[class*="_Question_"]>div>div>div>div')];
 
-    const elements = document.querySelectorAll('[class*="_Question_"]>div>div>div>div');
-
-    elements.forEach((element) => {
-      text += `\n- ${element.textContent}`;
-    });
+    const text = elements.map((element) => `-${element.textContent}`).join('');
 
     return text;
   });
