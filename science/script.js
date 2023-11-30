c032c1f0ee729fe81dce4b72c02c545fc41a3a7d
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-11-30 00:31:32 +0000

    Select more to add more to the question

--- science/script.js
+++ science/script.js
@@ -12,8 +12,17 @@ document.getElementById('selectTextButton').addEventListener('click', async func
 document.getElementById('copyTextButton').addEventListener('click', async function() {
   // TODO: Fix broken escapes.
   // e.g. `Ã—{\times }Ã—`
+  //
+  // TODO: Reduce selectiveness.
+  // e.g. Don't select `Zoom`
   const result = await execute(function() {
-    const text = document.querySelectorAll('[class*="_Question_"]>div>div>div>div')[0].textContent;
+    let text = "";
+
+    const elements = document.querySelectorAll('[class*="_Question_"]>div>div>div>div');
+
+    elements.forEach((element) => {
+      text += `\n- ${element.textContent}`;
+    });
 
     return text;
   });
