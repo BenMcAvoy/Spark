7c934d9a3f5db0e7d42252c236e7d0ae1e2b2199
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-11-30 01:40:52 +0000

    Update IDs in script

--- science/script.js
+++ science/script.js
@@ -3,13 +3,13 @@ import { showAlert } from '/static/modal.js';
 
 console.log("Loaded Spark for Science");
 
-document.getElementById('selectTextButton').addEventListener('click', async function() {
+document.getElementById('copyPastePatch').addEventListener('click', async function() {
   execute(function() {
     document.body.style.userSelect = "text";
   })
 });
 
-document.getElementById('copyTextButton').addEventListener('click', async function() {
+document.getElementById('getQuestion').addEventListener('click', async function() {
   // TODO: Fix broken escapes.
   // e.g. `Ã—{\times }Ã—`
   //
